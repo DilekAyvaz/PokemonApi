bf2560244cfb75c1c8d38f7e986ea9cf1cec28ce
DİLEK <dilekayvaz0@gmail.com>
2026-02-26 21:46:11 +0300

    Add Pokémon battle functionality and damage calculation

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from pstats import Stats
 import requests
 from urllib3 import response
+from tkinter import *
 
+#Fonksiyonlar
+# Pokémon verisini API'den çekme fonksiyonu
 def get_pokemon(name):
+    # API URL'ini oluşturuyoruz (girilen ismi küçük harfe çeviriyoruz)
     url=f"https://pokeapi.co/api/v2/pokemon/{name.lower()}"
     response= requests.get(url)
 
@@ -24,4 +28,56 @@ def get_pokemon(name):
 
 pikachu = get_pokemon("pikachu")
 print(pikachu)
+
+
+
+#Hasar Hesaplama 
+def calculate_damage(attacker ,defender ):
+    damage = attacker ["attack"] - (defender["defense"]/2)
+    #hasar minimum ve 1 olmalı -li olmaması için ve tam sayıya çevirdik.
+    return max(1, int(damage))
+
+# ----------------------------------------
+# Savaş fonksiyonu
+# ----------------------------------------
+def battle():
+    text_output.delete("1.0",END) #önceki sonucu temizliyoruz
+    p1_name = entry_pokemon1.get()
+    p2.name =entry_pokemon2.get()
+
+    p1= get_pokemon(p1_name)
+    p2= get_pokemon(p2_name)
+
+    if not p1 or not p2:
+        text_output.insert(END, "Hatalı Pokemon adi!\n")
+        return
+
+    hp1 = p1["hp"]# p1’in başlangıç HP’si
+    hp2 = p2["hp"]# p2’in başlangıç HP’si
+
+    if p1["speed"] > p2["speed"]:
+        first, second = p1, p2
+        hp_first, hp_second = hp1, hp2
+    else:
+        first, second = p2, p1
+        hp_first, hp_second = hp2, hp1
+
+    text_output.insert(END, f"İlk saldıran: {first['name']}\n\n")
+
+    while hp_first > 0 and hp_second > 0:
+        dmg = calculate_damage(first, second)
+        hp_second -= dmg
+        text_output.insert(END, f"{first['name']} {dmg} hasar verdi! ({second['name']} HP: {max(0, hp_second)})\n")
+
+        if hp_second <= 0:
+            text_output.insert(END, f"\n🏆 Kazanan: {first['name']}!\n")
+            return
+
+        dmg = calculate_damage(second, first)
+        hp_first -= dmg
+        text_output.insert(END, f"{second['name']} {dmg} hasar verdi! ({first['name']} HP: {max(0, hp_first)})\n\n")
+
+        if hp_first <= 0:
+            text_output.insert(END, f"\n🏆 Kazanan: {second['name']}!\n")
+            return
 
